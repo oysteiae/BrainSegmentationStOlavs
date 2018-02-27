5681cfe84cff276ede3116246b5a3ba557e9aaaf
oysteiae <oysteiae@stud.ntnu.no>
2018-02-27 17:46:35 +0100

    Started work on an argumentparser

--- SkullStripping/SkullStripping.py
+++ SkullStripping/SkullStripping.py
@@ -4,6 +4,7 @@ import helper
 from CNN.Build3DCNN import build_3DCNN
 from Unet.Trainer3DUnet import Trainer3DUnet
 from Callbacks.Logger import LossHistory
+import argparse
 
 # TODO: Look at data Augmentation
 # TODO: Write your own function for finding the optimal input size.
@@ -14,16 +15,31 @@ from Callbacks.Logger import LossHistory
 # TODO Fix voxel size output
 # TODO reshape input to same voxel size?
 def main():
-    #trainer = Trainer3DCNN(["D:\\MRI_SCANS\\data"], ["D:\\MRI_SCANS\\labels"], "trained_on_oasis_tested_on_lbpa40", build_CNN, using_sparse_categorical_crossentropy=False, use_cross_validation=False)
-    #predictor = Predictor3DCNN("models/batchsize21", ["D:\\MRI_SCANS\\predict"], build_CNN, using_sparse_categorical_crossentropy=False)
-    #predictor.predict()
-    #unetTrainer = build_3DUnet((144, 144, 144, 1))
-    #model = Trainer3DCNN()
-    #test = model.build_model()
-    unet = Trainer3DUnet((40, 40, 40, 1))
-    #unet = Trainer3DUnet((176, 208, 176, 1))
+    parser = argparse.ArgumentParser(description='Module for training a model or predicting using an existing model')
+    parser.add_argument('--mode', dest='mode', required=True, type=str, nargs='+', help='Specify if training or predicting')
+    parser.add_argument('--arc', dest='arc', required=True, type=str, nargs='+', help='Specify which arcitecture')
+    parser.add_argument('--savename', dest='save_name', required=True, type=str, nargs='+', help='Path to the corresponding labels')
+    parser.add_argument('--data', dest='data', required=False, type=str, nargs='+', help='Path to the data')
+    parser.add_argument('--labels', dest='labels', required=False, type=str, nargs='+', help='The save name of the model')
+    args = parser.parse_args()
     
-    #unet.train(["/localdata/OASIS/data"], ["/localdata/OASIS/data"], 1000, "unet_test", batch_size=1)
-    unet.train(["D:\\MRI_SCANS\\data"], ["D:\\MRI_SCANS\\labels"], 1000, "unet_test", batch_size=4)
+    if(args.mode == 'train'):
+        if(args.data is None or args.labels is None):
+            parser.error("Requires data and labels")
+        elif(args.arc == 'unet'):
+            unet = Trainer3DUnet((40, 40, 40, 1))
+            unet.train([args.data], [args.labels], 1000, args.save_name)        
+        elif(args.arc == 'cnn'):
+            model = Trainer3DCNN()
+            model.train([args.data], [args.labels], 1000, args.save_name)
+    if(args.mode == 'test'):
+        if(args.data is None or args.labels is None):
+            parser.error("Requires data and labels")
+        elif(args.arc == 'unet'):
+            unet = Predictor3DUnet((40, 40, 40, 1))
+            unet.train([args.data], [args.labels], 1000, args.save_name)        
+        elif(args.arc == 'cnn'):
+            model = Trainer3DCNN()
+            predictor = Predictor3DCNN(args.save_name, args.data)
 
 main()
