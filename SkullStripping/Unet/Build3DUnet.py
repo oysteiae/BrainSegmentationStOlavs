f54a5dae0867ca09876e5f5ac4743c07c7b74d3a
oysteiae <oysteiae@stud.ntnu.no>
2018-04-08 20:19:10 +0200

    weefr

--- SkullStripping/Unet/Build3DUnet.py
+++ SkullStripping/Unet/Build3DUnet.py
@@ -64,7 +64,7 @@ def build_3DUnet(input_shape, gpus, use_upsampling=False, initial_learning_rate=
     conv15 = Conv3D(filters = 2, kernel_size = 1, strides = stride)(conv14)
     act = Activation('softmax')(conv15)
     
-    loss_function = 'softmax'
+    loss_function = 'kld'
     
     # Support for training on multiple gpus.
     if(gpus > 1):
